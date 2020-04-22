65043723bf6d54c894d0397d4450ba5ce1473fe1
cass-codes <casseia93@gmail.com>
2020-04-22 10:01:47 -0700

    adding basic logEvent logic

--- src/counter.ts
+++ src/counter.ts
@@ -1,21 +1,34 @@
 const MAX_TIME = 5*60 //5 minute max in seconds
 
 export class Counter {
-  currentCount: number = 1;
+  // currentCount: number = 1;
 
-  logEvent() 
-  {
-    this.currentCount++;
+  counterMap = new Map<number, number>();
+
+
+  logEvent() {
+    // this.currentCount++;
     // the above doesn't work because what we really want to know is /when/ something happened
+
+    var now = Math.floor(Date.now() / 1000); //converting to a second rather than a millisecond
+    console.log('now looks like: ', now)
+
+    if (this.counterMap.has(now)) {
+      const current = this.counterMap.get(now);
+      this.counterMap.set(now, current + 1);
+    } else {
+      this.counterMap.set(now, 1);
+    }
+    
   }
 
-  getCount(time: number)
+  getCount(time: number = MAX_TIME)
   {
     //handle things if they ask for all time (no param) or a length longer than the max time
-    if(!time || time > MAX_TIME)
+    if(time > MAX_TIME)
       time = MAX_TIME
     
-    
+    console.log('Current Map: ', this.counterMap)
     
   }
 }
--- test/src/counter.test.ts
+++ test/src/counter.test.ts
@@ -10,7 +10,10 @@ describe('logging 1 event', () => {
   });
 
   it('returns 0 when asking outside the time frame', async () => {
-      const result = await sayGoodbye();
-      expect(result).to.equal('goodbye');
+    //TODO: Implement
+    const myCounter = new Counter();
+    myCounter.logEvent();
+    var result = myCounter.getCount(); // no params means all time
+    expect(result).to.equal(1);
   });
 });
